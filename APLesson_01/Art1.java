addd52dec96478ed02c8fb0b4381e049beb03de4
mannb7803 <bmann2525@Gmail.com>
2016-09-02 10:46:21 -0700

    Added Files
    Project_1

---
+++ APLesson_01/Art1.java
@@ -0,0 +1,15 @@
+public class Art1
+{
+	public static void main (String args[])
+	{
+		
+	// eyes	
+	System.out.println("____          ____\n|  x  |        |  x  |");
+	// nose
+	System.out.println("         |__|");
+	// mouth
+	System.out.println("_____________");
+		
+		
+	}
+}
