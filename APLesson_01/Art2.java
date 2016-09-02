5cdc6e13fb4e779af55d458b24de05145229b650
mannb7803 <bmann2525@Gmail.com>
2016-09-02 11:15:27 -0700

    Updates

--- APLesson_01/Art2.java
+++ APLesson_01/Art2.java
@@ -1,16 +1,17 @@
 public class Art2{
 	public static void main (String args[])
 	{
-		System.out.println("++" + "                                ++");
-		System.out.println("++" + "                               ++");			
-		System.out.println("++" + "                              ++");
-		System.out.println("++" + "                             ++");
-		System.out.println("++" + "                            ++");
-		System.out.println("++" + "                           ++");
-		System.out.println("++" + "                           ++");
-		System.out.println("++" + "                             ++");
-		System.out.println("+++++++++++" + "                      ++ ");
-		System.out.println("+++++++++++" + "                        +++");
-					
+		System.out.println("-                /==+=+=+=+=+==\\               -");
+		System.out.println("-               / -------------------------- -\\               -");
+		System.out.println("-              /  +++----++-++++-+-- \\              -");
+		System.out.println("-             / -++-++--++-++----+--- \\             -");
+		System.out.println("             | --++- ++-++-++----+--   |	           -");
+		System.out.println("-            \\ -++---++++-++----+++/              -");
+		System.out.println("-             \\ ------------------------------/               -");
+		System.out.println("-              \\ ----------------------------/                -");
+		System.out.println("-               \\=============/ -");
+		
+	//bad football
+	
 	}
 }	
