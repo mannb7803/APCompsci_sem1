48c6f0f74b123b251b3aeed675dc8195709c1133
mannb7803 <bmann2525@Gmail.com>
2016-09-09 11:04:10 -0700

    Lesson2
    Absent on day it was assigned

---
+++ APLesson_02/MAth.java
@@ -0,0 +1,11 @@
+public class MAth{
+public static void main (String args[])
+	{
+	int l = 5;
+	int w = 3;
+	int h = 10;
+		System.out.println("10 multiplied by 10 is " + (10*10));
+		System.out.println("\nBen Mann\n" + "4832 almondwoodway\n" + "San Diego\n" + "92130\n");
+		System.out.println(2*(w*l + h*l + h*w));
+	}
+}
