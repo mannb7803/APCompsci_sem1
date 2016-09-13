2be287c1834dc58b62a22e51b9ce35efa6fa62f4
mannb7803 <bmann2525@Gmail.com>
2016-09-13 11:39:11 -0700

    Update
    It lost my code somehow, redid

---
+++ APLesson_02/MAth.java
@@ -0,0 +1,11 @@
+public class MAth{
+	public static void main (String args[])
+	{
+		//not needed declerations but whatever
+		int side = 10;
+		int area = 100;
+		System.out.println("7 multiplied by 2 is " + (2*7));
+		System.out.println("\nBen Mann\n 4832almondwoodway\n San diego\n 92130");
+		System.out.println("\nThe surface area of this cube = " + (6*area));
+	}
+}
