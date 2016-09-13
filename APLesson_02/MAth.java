7e8e38606d8959620ad1f30b4a4f61ea9db2443f
mannb7803 <bmann2525@Gmail.com>
2016-09-13 11:41:17 -0700

    APLesson_02
    update, github lost my code

--- APLesson_02/MAth.java
+++ APLesson_02/MAth.java
@@ -7,5 +7,6 @@ public class MAth{
 		System.out.println("7 multiplied by 2 is " + (2*7));
 		System.out.println("\nBen Mann\n 4832almondwoodway\n San diego\n 92130");
 		System.out.println("\nThe surface area of this cube = " + (6*area));
+		
 	}
 }
