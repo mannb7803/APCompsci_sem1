610e7a9fd0cdc1b39b881fcec0e4a07d71ee893a
mannb7803 <bmann2525@Gmail.com>
2016-11-04 10:45:30 -0700

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class factorial
+{
+	public static void main (String args[])
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a number");
+		int num = kb.nextInt();
+		int fac = 1;
+		for (int i = 1; i <= num; i++)
+		{
+			System.out.println(fac * i);
+		}
+	}
+}
