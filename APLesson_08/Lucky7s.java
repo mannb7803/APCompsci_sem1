262e4cdec03b26e5e304eb149f34a5c35434137f
mannb7803 <bmann2525@Gmail.com>
2016-11-08 12:10:26 -0800

    asdsad
    asdasdsa

---
+++ APLesson_08/Lucky7s.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Lucky7s
+{
+	public static void main (String args[])
+	{
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Please enter a number");
+		int num = kb.nextInt();
+		
+	}
+	public static int (int num)
+	{
+		if (num > 0)
+		{
+			if 
+			{
+				
+			}
+		}
+	
+	}
+}
