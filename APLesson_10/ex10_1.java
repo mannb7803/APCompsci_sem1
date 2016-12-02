574f4bdba34f8588f549bd4dc82f637a16cadb3f
mannb7803 <bmann2525@Gmail.com>
2016-12-02 12:14:42 -0800

    uogl

---
+++ APLesson_10/ex10_1.java
@@ -0,0 +1,38 @@
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ex10_1
+{
+	public static void main (String args[])
+	{
+		
+		Integer [] numbers = {2, 6, 8, 9, 10, 12, 13, 15, 17, 24, 55, 66, 78, 77, 79};
+		ArrayList<Integer> num = new ArrayList<>(Arrays.asList(numbers));
+		System.out.println(num);
+		System.out.println(removePrimes(num));
+	}
+	public static int gFactor(int x)
+	{
+		for(int i = 2; i < x; i++)
+		{
+			if (x % i == 0)
+			{
+				return 1;
+			}
+			return 0;
+		}
+		return 0;
+	}
+	public static void removePrimes(ArrayList<Integer> num)
+	{
+		for(int i = 0; i < num.size(); i++)
+		{
+			if(gFactor(num.get(i)) == 0);
+			{
+				num.remove(i);
+				i--;
+			}
+		}
+	}
+}
